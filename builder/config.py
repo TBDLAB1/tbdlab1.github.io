0a1f9efea990300192378acddb3909247974a4fa
Hongsu Byun <byhs@sogang.ac.kr>
2026-07-04 15:28:25 +0900

    Update API_KEY in config.py

--- builder/config.py
+++ builder/config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUILD_PATH = os.path.join(BASE_PATH, 'docs')
-API_KEY = os.getenv('INPUT_API_KEY', '') or 'AIzaSyDOh00sVcAmUVoVcUWCJiRx5aIq40XJs3o'
+API_KEY = os.getenv('INPUT_API_KEY', '') or '573caf0af340ca84b18f5926de9aff5257262661'
 
 DATA_URL = 'https://docs.google.com/spreadsheets/d/1SaAs1CaSkPwv4Ktg4z0P7ETSZl1o7yW0qtFJakNh7lM/edit?usp=sharing'
